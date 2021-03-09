c50c021b23bf3ae17329bfa137732855cf05dd4a
Akihiro <marsstay0729@gmail.com>
2021-03-09 11:13:59 +0900

    Add addTodo function

---
+++ app.js
@@ -0,0 +1,42 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(e) {
+  e.preventDefault()
+  // Create div.todo
+  const todoDiv = document.createElement('div')
+  todoDiv.classList.add('todo')
+
+  // Create li
+  const newTodo = document.createElement('li')
+  newTodo.innerText = 'hey'
+  newTodo.classList.add('todo-item')
+  // Insert li in div.todo
+  todoDiv.appendChild(newTodo)
+
+  // // Create button
+  // const button = document.createElement('button')
+
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement('button')
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  completedButton.classList.add('complete-btn')
+  // Insert li in div.todo
+  todoDiv.appendChild(completedButton)
+
+  // CHECK TRASH BUTTON
+  const trashButton = document.createElement('button')
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add('delete-btn')
+  // Insert li in div.todo
+  todoDiv.appendChild(trashButton)
+
+  // Insert div.todo in ul.todo-list
+  todoList.appendChild(todoDiv)
+}
